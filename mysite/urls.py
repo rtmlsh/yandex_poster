3ca807284030caedb2bfe46e1225e51da9535e94
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-05-18 20:51:30 +0300

    Add blank start page

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from yandex_poster import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.start_page)
 ]
