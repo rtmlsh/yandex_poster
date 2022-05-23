88f7af0b354b107b569899bd4d703362b8549492
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-05-23 21:29:21 +0300

    Create places app

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 from yandex_poster import views
 
 
@@ -22,3 +26,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.start_page)
 ]
+
+urlpatterns += [
+    path('places/', include('places.urls')),
+    path('', RedirectView.as_view(url='/places/', permanent=True))
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
