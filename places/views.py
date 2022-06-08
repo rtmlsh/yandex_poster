003f904039d98167ceb4691150b4911fcbd04e37
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-06-08 22:14:29 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -4,19 +4,20 @@ from places.models import Place, ImagePlace
 
 def serialize_place(place):
     serialized_place = {
-        "title": place.title,
-        "place_id": place.place_id,
-        "coordinates": {
-            "lng": float(place.longitude),
-            "lat": place.latitude
+            'type': 'Feature',
+            'geometry': {'type': 'Point', 'coordinates': [place.longitude, place.latitude]},
+            'properties': {'title': place.title, 'placeId': place.place_id, 'detailsUrl': 'test'}
         }
-    }
+
     return serialized_place
 
 
 def get_event(request):
     places = Place.objects.all()
     context = {
-        'place_specifies': [serialize_place(place) for place in places]
+        'place_features': {
+            'type': 'FeatureCollection',
+            'features': [serialize_place(place) for place in places]
+        }
     }
     return render(request, "index.html", context)
